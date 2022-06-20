02ed78f5c2188c9d2569ca269414d34c6617cbf5
Hemang Ranjan <74303960+Hemang417@users.noreply.github.com>
2022-06-21 04:58:11 +0530

    Get a free quote

---
+++ get-free-qoute.php
@@ -0,0 +1,282 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Get a Free Quote</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="./images/circle-logo.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+  <link href="./assets/css/get-free-qoute.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+   <!-- ======= Header ======= -->
+  <header id="header" class="d-flex align-items-center ">
+    <div class="container-fluid d-flex align-items-center justify-content-lg-between">
+
+      <h1 class="logo me-auto me-lg-0"><a href="index.html"><img src="./images/seawave.png"></a></h1>
+      
+      <nav id="navbar" class="navbar order-last order-lg-0">
+        <ul>
+          <li><a class="nav-link scrollto" href="./index.html">Home</a></li>
+          <li><a class="nav-link scrollto" href="./about-us.html">About Us</a></li>
+          <li class="dropdown services-hidden active"><a href="#services"><span>Services</span> <i class="bi bi-chevron-down down-arrow"></i><i class="bi bi-chevron-up up-arrow"></i></a>
+            <ul>
+              <li><a href="./custom-clearance.html">Custom Clearance</a></li>
+              <li><a href="./freight-forwarding.html">Freight Forwarding</a></li>
+              <li><a href="./land-freight.html">Land Freight</a></li>
+              <li><a href="./warehousing.html">Warehousing</a></li>
+              <li><a href="./exim-consultancy.html">Exim Consultancy</a></li>
+              <li><a href="./logistics-design.html">Logistics Design</a></li>
+            </ul>
+          </li>
+          <li class="dropdown"><a href="#services"><span>Services</span> <i class="bi bi-chevron-down down-arrow"></i><i class="bi bi-chevron-up up-arrow"></i></a>
+            <ul>
+              <li><a href="./custom-clearance.html">Custom Clearance</a></li>
+              <li class="dropdown dropdown1"><a href="./freight-forwarding.html"><span>Freight Forwarding</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+
+                  <li><a href="./freight-forwarding.html#airfreight">Air Freight </a></li>
+                  <li><a href="./freight-forwarding.html#seafreight">Sea Freight </a></li>
+
+                </ul>
+              </li>
+              <li class="dropdown dropdown1"><a href="./land-freight.html"><span>Land Freight</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+
+                  <li><a href="./land-freight.html#transportation">Transportation </a></li>
+                  <li><a href="./land-freight.html#vehicleoperation">Vehicle Operation </a></li>
+
+                </ul>
+              </li>
+              <li><a href="./warehousing.html">Warehousing</a></li>
+              <li><a href="./exim-consultancy.html">Exim Consultancy</a></li>
+              <li><a href="./logistics-design.html">Logistics Design</a></li>
+
+            </ul>
+          </li>
+
+          <li><a class="nav-link scrollto" href="./index.html#specialization">Specialization</a></li>
+          <li><a class="nav-link scrollto" href="./index.html#contact">Contact Us</a></li>
+
+        </ul>
+        
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+      <div class="header-social-links d-flex align-items-center">
+        <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+        <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+        <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></i></a>
+        <a href="#getfreeqoute" class="btn-get-started scrollto">Get A Free Quote</a>
+
+      </div>
+
+    </div>
+  </header><!-- End Header -->
+
+  <main id="main">
+
+    <section id="image-section"> 
+      <div class="image-container">
+        <h1>Get A Free Quote</h1>
+      </div>
+    </section>
+
+    <section id="getfreeqoute" class="contact">
+      <div class="col-lg-2"></div>
+      <div class="col-lg-8 mt-5 mt-lg-0">
+
+        <form method="POST" role="form" class="php-email-form">
+          <div class="row">
+            <div class="col-md-6 form-group">
+              <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
+            </div>
+            <div class="col-md-6 form-group mt-3 mt-md-0">
+              <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required>
+            </div>
+          </div>
+          <div class="form-group mt-3">
+            <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required>
+          </div>
+          <div class="form-group mt-3">
+            <textarea class="form-control" name="message" rows="5" placeholder="Message" required></textarea>
+          </div>
+          <div class="my-3">
+            <div class="loading">Loading</div>
+            <div class="error-message"></div>
+            <div class="sent-message">Your message has been sent. Thank you!</div>
+          </div>
+          <div class="text-center"><button type="submit">Send Message</button></div>
+        </form>
+
+      </div>
+    </section>
+
+  </main><!-- End #main -->
+
+  <?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require('PHPMailer/Exception.php');
+require('PHPMailer/PHPMailer.php');
+require('PHPMailer/SMTP.php');
+
+  if (isset($_POST['Login'])){
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+  $mail = new PHPMailer(true);
+
+
+  try {
+    //Server settings
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = "";                     //SMTP username
+    $mail->Password   = "";                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('hemangranjan417@gmail.com', 'Seawave Forwarding & Logistics');
+    $mail->addAddress('hemangranjan417@gmail.com', 'Joe User');     //Add a recipient
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Get a Free Quote';
+    $mail->Body    = "Name: $name<br> Email: $email<br> Message: $message";
+    // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+
+    echo "<script>alert('Message has been sent')</script>";
+    } catch (Exception $e) {
+    echo "<script>alert('Message could not be sent. Mailer Error: {$mail->ErrorInfo}')</script>";
+    }
+
+  }
+
+  ?>
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+
+    <div class="footer-top">
+
+      <div class="container">
+
+        <!-- <div class="row">
+          <div class="col-lg-6">
+            <h3>Valera</h3>
+            <p>Et aut eum quis fuga eos sunt ipsa nihil. Labore corporis magni eligendi fuga maxime saepe commodi placeat.</p>
+          </div>
+        </div>
+        <div class="col-lg-6 pt-4">
+          <h3>Valera</h3>
+          <p>Et aut eum quis fuga eos sunt ipsa nihil. Labore corporis magni eligendi fuga maxime saepe commodi placeat.</p>
+        </div> -->
+        <div class="row content">
+          <div class="col-lg-4 pt-4 pt-lg-0 left-side-footer">
+            <h5>At SEAWAVE, we define ourselves as:</h5><br>
+             <ul>
+              <li>S: Sea</li>
+              <li>E: Earth</li>
+              <li>A: Air</li>
+              <li>W: Wave</li>
+              <li>A: Achieve</li>
+              <li>V: Vision</li>
+              <li>E: Exactly</li>
+            </ul>
+        </div>
+  
+          <div class="col-lg-4">
+            <img src="./images/seawave-logo1.png" style="width: 75%;">
+          </div>
+          <div class="col-lg-4 right-side-footer">
+            <h5>
+              Seawave Forwarding & Logistics Pvt. Ltd.
+            </h5>
+            <p>
+              Address: <br>
+              Raheja District II Edison (Building No.1), Unit No. 232, Second Floor Plot No. GEN-1/2/C Part, D-Block, TTC Industrial Area, Juinagar, Navi Mumbai, Maharashtra 400705
+            </p>
+            <p>
+              Contact Number : +91-22-68727905
+            </p>
+            <p>
+              info@seawave.in<br>
+              Mon - Sat 9.30 - 19.00
+            </p>
+            
+          </div>         
+
+    </div>
+      </div>
+
+
+        <div class="social-links">
+          <!-- <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a> -->
+          <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+          <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+          <!-- <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a> -->
+          <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+        </div>
+
+      </div>
+    </div>
+
+    <div class="container footer-bottom clearfix">
+      <div class="copyright">
+        &copy; Copyright <strong><span>SEAWAVE</span></strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+        Developed by <a href="https://bootstrapmade.com/">SEAWAVE</a>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/purecounter/purecounter.js"></>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <!-- <script src="assets/js/validate.js"></script> -->
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+ 
+</body>
+
+</html>
